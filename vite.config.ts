e47a5ad9ac1b1fbea171fa7653fc80f826c946ae
JungBrasil <jungbrasiloficial@gmail.com>
2025-11-18 15:24:09 -0400

    Update Vite config with base and comments
    Added base configuration and comment for API_KEY access.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-﻿import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   define: {
+    // Isso permite que o código acesse process.env.API_KEY mesmo no navegador
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
   },
 });
