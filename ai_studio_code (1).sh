4ee855c1270ddbc36305523dff60fac2192b3960
JungBrasil <jungbrasiloficial@gmail.com>
2025-11-18 14:04:36 -0400

    Add files via upload

---
+++ ai_studio_code (1).sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Subindo XControl.ia para produção"
+git branch -M main
+git remote add origin https://github.com/JungBrasil/Xcontrol.ia.git
+git push -u origin main
